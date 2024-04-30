fb467043756a0c8a088b37b27f2c577d8e98ea19
Marina <nozdryakovamarina94@gmail.com>
2024-04-30 15:18:04 +0400

    edge structure

--- graph_list.cpp
+++ graph_list.cpp
@@ -8,5 +8,22 @@
 using namespace std;
 
 namespace graph {
-	
+	template<typename Vertex, typename Distance = double>
+	struct Edge {
+		Vertex from;
+		Vertex to;
+		Distance data;
+
+		Edge() : from(Vertex()), to(Vertex()), data(0) {}
+		Edge(Vertex f, Vertex t) : from(f), to(t), data(0) {}
+		Edge(Vertex f, Vertex t, Distance d) : from(f), to(t), data(d) {}
+
+		void print() {
+			cout << "From the vertex: " << from << " | To the vertex: " << to << " | Distance: " << data << endl;
+		}
+	};
+	template<typename Vertex, typename Distance = double>
+	class Graph {
+	private:
+	};
 }
--- graph_tests.cpp
+++ graph_tests.cpp
@@ -6,4 +6,26 @@
 using namespace graph;
 using namespace std;
 
+TEST(EdgeTests, Constructor) {
+	Edge<int> a(1, 2);
+	EXPECT_EQ(a.from, 1);
+	EXPECT_EQ(a.to, 2);
+	EXPECT_EQ(a.data, 0);
+}
 
+TEST(EdgeTests, Constructor2) {
+	Edge<int, int> a(1, 2, 7);
+	EXPECT_EQ(a.from, 1);
+	EXPECT_EQ(a.to, 2);
+	EXPECT_EQ(a.data, 7);
+}
+
+TEST(EdgeTests, Print) {
+	Edge<int, int> a(6, 2, 12);
+	a.print();
+}
+
+TEST(EdgeTests, Print2) {
+	Edge<int, int> a(6, 2);
+	a.print();
+}
